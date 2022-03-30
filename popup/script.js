3aeb58a5c53963b0a926a498d332c1d590c103e5
Deven Speyrer <devenspeyrer@gmail.com>
2022-03-30 13:04:04 -0600

    complete parse youtube data function

--- popup/script.js
+++ popup/script.js
@@ -5,10 +5,6 @@ const loader = document.getElementById("loadingContainer"),
   userListWrapper = document.getElementById("userList-wrapper"),
   headerPrefix = "__YTSAVE_HEADER_" + Math.random().toString(36).slice(2) + "_",
   sortActive = (a, b) => (a.active ? -1 : b.active ? 1 : 0),
-  headerReducer = (a, i) => {
-    a[i.name] = i.value;
-    return a;
-  },
   preloadImage = (url) =>
     new Promise((resolve) => {
       let img = new Image();
@@ -30,9 +26,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
         let name = i.name.replace(headerPrefix, "");
 
         requestHeaders = requestHeaders.filter(
-          (j) =>
-            j.name.toLowerCase() != name.toLowerCase() &&
-            j.name.toLowerCase() != i.name.toLowerCase()
+          (j) => j.name.toLowerCase() != name.toLowerCase() && j.name.toLowerCase() != i.name.toLowerCase()
         );
         requestHeaders.push({ name, value: i.value });
       });
@@ -40,9 +34,9 @@ browser.webRequest.onBeforeSendHeaders.addListener(
     return { requestHeaders };
   },
   {
-    urls: ["https://www.youtube.com/*"],
+    urls: ["https://www.youtube.com/*"]
   },
-  ["requestHeaders", "blocking", "extraHeaders"]
+  ("requestHeaders blocking" + (firefox ? "" : " extraHeaders")).split(" ")
 );
 
 function hideUserlist() {
@@ -52,7 +46,11 @@ function hideUserlist() {
   setTimeout(() => (userList.innerHTML = ""), cssToMs(getComputedStyle(content).transitionDuration));
 }
 
-function showUserlist(users) {
+async function showUserlist() {
+  let users = await getUsers();
+
+  await Promise.all(users.flatMap((user) => user.accounts.map((account) => preloadImage(account.icon))));
+
   users.forEach((i) => {
     let email = document.createElement("span");
     email.className = "email";
@@ -75,24 +73,13 @@ function showUserlist(users) {
   addAccount.id = "addNew";
   addAccount.className = "user";
   addAccount.innerHTML = `<img src="adduser.svg" /> Add another account`;
-  addAccount.onclick = () => login(true);
+  addAccount.onclick = login;
   userList.appendChild(addAccount);
   userListWrapper.style.opacity = "1";
   userListWrapper.style.pointerEvents = "auto";
   activeIcon.style.opacity = "0";
 }
 
-
-async function loadUserData(users) {
-  await Promise.all(
-    users.map((user) =>
-      Promise.all(user.accounts.map((account) => preloadImage(account.icon)))
-    )
-  );
-
-  activeIcon.src = users[0].accounts[0].icon;
-  activeIcon.onclick = () => showUserlist(users);
-}
 userListWrapper.onclick = hideUserlist;
 
 function changeUser(endpoint) {
@@ -101,21 +88,19 @@ function changeUser(endpoint) {
   hideUserlist();
   fetch(endpoint, {
     headers: {
-      [headerPrefix + "Origin"]: "https://www.youtube.com",
-    },
+      [headerPrefix + "Origin"]: "https://www.youtube.com"
+    }
   }).then(load);
 }
 
 async function getYoutubeData(url) {
   let data = await fetch("https://www.youtube.com/" + url, {
-    credentials: "include",
+    credentials: "include"
   }).then((res) => res.text());
 
   return {
-    initialData: JSON.parse(
-      "{" + data.match(/(?<=var ytInitialData = {).*?(?=};)/s) + "}"
-    ),
-    context: getInternalApiParams(data),
+    initialData: JSON.parse("{" + data.match(/(?<=var ytInitialData = {).*?(?=};)/s) + "}"),
+    context: getInternalApiParams(data)
   };
 }
 
@@ -128,67 +113,93 @@ function getInternalApiParams(data) {
     }, {});
 }
 
-async function getSubscriptionsFeed(continuation, context) {
-  let data, continuationData;
-  if (continuation) {
+function parseSubscriptionsData(data) {
+  return {
+    token: data[data.length - 1].continuationItemRenderer
+      ? data.pop().continuationItemRenderer.continuationEndpoint.continuationCommand.token
+      : null,
+    items: data
+      .flatMap((i) => i.itemSectionRenderer.contents[0].shelfRenderer.content.gridRenderer.items)
+      .map(({ gridVideoRenderer: i }) => {
+        let overlayData = i.thumbnailOverlays.reduce(Object.assign, {}),
+          badges = (i.ownerBadges || [])
+            .concat(i.badges || [])
+            .map((i) => i.metadataBadgeRenderer.style.replace("BADGE_STYLE_TYPE_", "")),
+          live = badges.includes("LIVE_NOW");
+
+        return {
+          id: i.videoId,
+          thumbnail: `https://i.ytimg.com/vi/${i.videoId}/maxresdefault.jpg`,
+          title: i.title.runs[0].text,
+          live,
+          published: live ? null : i.publishedTimeText.simpleText,
+          viewCount: live ? i.viewCountText.runs.reduce((a, i) => a + i.text, "") : i.viewCountText.simpleText,
+          animatedThumbnail: i.richThumbnail
+            ? i.richThumbnail.movingThumbnailRenderer.movingThumbnailDetails.thumbnails.pop().url
+            : null,
+          channel: {
+            name: i.shortBylineText.runs[0].text.trim(),
+            id: i.shortBylineText.runs[0].navigationEndpoint.browseEndpoint.browseId,
+            icon: i.channelThumbnail.thumbnails[0].url,
+            verified: badges.includes("VERIFIED")
+          },
+          duration: live ? null : overlayData.thumbnailOverlayTimeStatusRenderer.text.simpleText,
+          progress: overlayData.thumbnailOverlayResumePlaybackRenderer
+            ? overlayData.thumbnailOverlayResumePlaybackRenderer.percentDurationWatched
+            : 0
+        };
+      })
+  };
+}
+
+async function getSubscriptionsFeed(token, context) {
+  if (token && context) {
     let response = await internalApiRequest(
       "POST",
       "browse",
       {
-        continuation,
+        continuation: token
       },
       context
     );
 
-    data =
-      response.onResponseReceivedActions[0].appendContinuationItemsAction
-        .continuationItems;
+    return parseSubscriptionsData(
+      response.onResponseReceivedActions[0].appendContinuationItemsAction.continuationItems
+    );
   } else {
     let response = await getYoutubeData("feed/subscriptions");
-    continuationData = response.context;
-    data =
-      response.initialData.contents.twoColumnBrowseResultsRenderer.tabs[0]
-        .tabRenderer.content.sectionListRenderer.contents;
+    return {
+      ...parseSubscriptionsData(
+        response.initialData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer
+          .contents
+      ),
+      etc: {
+        context: response.context,
+        userIcon:
+          response.initialData.topbar.desktopTopbarRenderer.topbarButtons.pop().topbarMenuButtonRenderer.avatar
+            .thumbnails[0].url
+      }
+    };
   }
-
-  return {
-    continuation: {
-      context: continuationData || context,
-      token: data[data.length - 1].continuationItemRenderer
-        ? data.pop().continuationItemRenderer.continuationEndpoint
-            .continuationCommand.token
-        : null,
-    },
-    items: data.flatMap(
-      (i) =>
-        i.itemSectionRenderer.contents[0].shelfRenderer.content.gridRenderer
-          .items
-    ),
-  };
 }
 
 async function sha1hash(data) {
-  return [
-    ...new Uint8Array(
-      await crypto.subtle.digest("SHA-1", new TextEncoder().encode(data))
-    ),
-  ]
+  return [...new Uint8Array(await crypto.subtle.digest("SHA-1", new TextEncoder().encode(data)))]
     .map((i) => i.toString(16))
     .join("");
 }
 
 async function apiAuthHeader() {
   let timestamp = Math.floor(new Date().getTime() / 1000);
-  return `SAPISIDHASH ${timestamp}_${await sha1hash(
-    timestamp + " " + SAPISID + " https://www.youtube.com"
-  )}`;
+  return `SAPISIDHASH ${timestamp}_${await sha1hash(timestamp + " " + SAPISID + " https://www.youtube.com")}`;
 }
 
 async function internalApiRequest(method, endpoint, body, context) {
   return await fetch(
     `https://www.youtube.com/youtubei/v1/${endpoint}?key=${context.INNERTUBE_API_KEY}&prettyPrint=false`,
     {
-      mode: "same-origin",
+      mode: "cors",
+      credentials: "include",
       headers: {
         Accept: "*/*",
         "Accept-Language": "en-US,en;q=0.5",
@@ -206,24 +217,21 @@ async function internalApiRequest(method, endpoint, body, context) {
         "Sec-Fetch-Site": "same-origin",
         Pragma: "no-cache",
         "Cache-Control": "no-cache",
-        [headerPrefix + "Referer"]:
-          "https://www.youtube.com/feed/subscriptions",
-        [headerPrefix + "Origin"]: "https://www.youtube.com",
+        [headerPrefix + "Referer"]: "https://www.youtube.com/feed/subscriptions",
+        [headerPrefix + "Origin"]: "https://www.youtube.com"
       },
       body: JSON.stringify({
         context: context.INNERTUBE_CONTEXT,
-        ...body,
+        ...body
       }),
-      method,
-      mode: "cors",
+      method
     }
   ).then((res) => res.json());
 }
 
-async function login(addAcc = false) {
+async function login() {
   await browser.tabs.create({
-    url:
-      "https://accounts.google.com/" + (addAcc ? "AddSession" : "ServiceLogin"),
+    url: "https://accounts.google.com/AddSession"
   });
 
   window.close();
@@ -231,21 +239,18 @@ async function login(addAcc = false) {
 
 async function getUsers() {
   let res = await fetch("https://www.youtube.com/getAccountSwitcherEndpoint", {
-    credentials: "include",
+    credentials: "include"
   });
 
   let resBody = await res.text();
   return JSON.parse(resBody.slice(5))
-    .data.actions[0].getMultiPageMenuAction.menu.multiPageMenuRenderer.sections.map(
-      (i) => i.accountSectionListRenderer
-    )
+    .data.actions[0].getMultiPageMenuAction.menu.multiPageMenuRenderer.sections.map((i) => i.accountSectionListRenderer)
     .map((i, index) => {
       let email = index
-        ? i.contents[0].accountItemSectionRenderer.header
-            .accountItemSectionHeaderRenderer.title.runs[0].text
+        ? i.contents[0].accountItemSectionRenderer.header.accountItemSectionHeaderRenderer.title.runs[0].text
         : i.header.googleAccountHeaderRenderer.email.simpleText;
 
-      accounts = i.contents[0].accountItemSectionRenderer.contents;
+      let accounts = i.contents[0].accountItemSectionRenderer.contents;
       accounts = (index ? accounts : accounts.slice(0, accounts.length - 1))
         .map(({ accountItem }) => {
           return {
@@ -254,20 +259,17 @@ async function getUsers() {
             icon: accountItem.accountPhoto.thumbnails[0].url,
             endpoint:
               "https://www.youtube.com" +
-              accountItem.serviceEndpoint.selectActiveIdentityEndpoint.supportedTokens.reduce(
-                (a, i) => {
-                  return { ...a, ...i };
-                },
-                {}
-              ).accountSigninToken.signinUrl,
-            byline: accountItem.accountByline.simpleText,
+              accountItem.serviceEndpoint.selectActiveIdentityEndpoint.supportedTokens.reduce((a, i) => {
+                return { ...a, ...i };
+              }, {}).accountSigninToken.signinUrl,
+            byline: accountItem.accountByline.simpleText
           };
         })
         .sort(sortActive);
       return {
         email,
         active: !!accounts.find((i) => i.active),
-        accounts,
+        accounts
       };
     })
     .sort(sortActive);
@@ -275,23 +277,24 @@ async function getUsers() {
 
 async function load() {
   await Promise.all([
-    getUsers()
-      .catch((e) => {
-        login();
-      })
-      .then(loadUserData),
     document.fonts.load("14px Roboto"),
     preloadImage("adduser.svg"),
     browser.cookies
       .get({
         url: "https://www.youtube.com",
-        name: "SAPISID",
+        name: "SAPISID"
       })
-      .then((cookie) => (SAPISID = cookie.value)),
+      .then((cookie) => (SAPISID = cookie.value))
+      .then(getSubscriptionsFeed)
+      .catch(login)
+      .then(({ token, items, etc }) => {
+        activeIcon.src = etc.userIcon;
+        x = [token, items, etc.context];
+      })
   ]);
 
-  loader.style.opacity = 0;
-  content.style.opacity = 1;
+  loader.style.opacity = "0";
+  content.style.opacity = "1";
 }
 
 load();
