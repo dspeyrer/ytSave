5403e3ab6b96a8f0bdc31ec1c5e7683f34d93fb8
Deven Speyrer <devenspeyrer@gmail.com>
2022-04-10 00:25:06 -0600

    Add svelte support

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,25 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
+import svelte from "rollup-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import css from "rollup-plugin-css-only";
 
 export default {
-  input: "src/script.js",
+  input: "src/main.ts",
   output: {
     sourcemap: true,
     format: "esm",
     file: "dist/js/bundle.js"
   },
   plugins: [
+    svelte({
+      preprocess: sveltePreprocess({ sourceMap: true }),
+      compilerOptions: {
+        dev: true
+      }
+    }),
+    css({ output: "bundle.css" }),
     resolve({
       browser: true
     }),
